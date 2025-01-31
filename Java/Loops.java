eb9c72b85f16b4780faa020f39a8e6b58ceaf596
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-01-31 11:35:41 +0530

    Update Loops.java

--- Java/Loops.java
+++ Java/Loops.java
@@ -1,21 +1,71 @@
 package com.Anjani;
 
+import java.util.Scanner ;
+
 public class Loops {
     public static void main(String[] args) {
-        int a = 10;
-        if (a==10) {
-            System.out.println("Hello World");
-        }
-//while loop
-        int count = 1;
-        while(count !=5) {
-            System.out.println(count);
-            count++;
+
+        /*
+        Basic 3 loops:
+        - For: If number of iterations is known
+        - While:
+        -Do-while:The loop will execute atleast once (Runs the body first then checks)
+
+        Syntax for for-loop:
+
+        for(initialization; conditions; increment/decrement){
+        //body
         }
 
-//For loop
-        for(int count = 1; count != 5; count++) {
-            System.out.println(count);
+
+        //Print numbers from 1 to 5
+
+        for(int num = 1; num<=5 ; num++){
+            System.out.println(num);
         }
+
+
+        //print numbers from 1 to n
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int num = 1; num<=n ; num++){
+          //  System.out.println(num + " ");
+            System.out.println("Hello World");
+        }
+        */
+
+        /*
+        while loops
+
+        Syntax:
+        while(conditions){
+             //body
+             }
+
+        int num = 1;
+        while(num<=5){
+            System.out.println(num);
+            num += 1;
+        }
+        */
+
+
+
+        /*
+        do - while loop:
+
+        Syntax: do{
+                 //body;
+                }while(condition)
+         */
+
+        int num = 1;
+        do{
+            System.out.println(num);
+            num++;
+        }while(num<=5);
+
     }
 }
