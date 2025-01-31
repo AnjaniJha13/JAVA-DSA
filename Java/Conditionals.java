5c9c6cffed35193a11c6f1f1b59928eb1bf93aa1
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-01-31 11:35:14 +0530

    Create Conditionals.java

---
+++ Java/Conditionals.java
@@ -0,0 +1,69 @@
+package com.Anjani;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        /*
+         Syntax of if statements:
+
+         if (boolean expression T or F){
+         //body;
+         }else{
+          //do this;
+          }
+
+         //if-else statements(control flow)
+
+        int salary = 25000;
+        if(salary>15000){
+            salary = salary + 2000;
+        }else{
+            salary = salary + 1000;
+        }
+        System.out.println(salary);
+    }
+}
+
+
+        // if-else-if ladder
+
+        int salary = 25000;
+
+        if (salary > 20000) {
+            salary += 3000;
+        } else if (salary > 10000) {
+            salary += 2000;
+        } else {
+            salary += 1000;
+        }
+        System.out.println(salary);
+
+
+
+        //Logical operators:
+        - AND(&&)
+        - OR(||)
+        - NOT(!)
+         */
+
+        int a = 10;
+        int b = 20;
+
+        //AND
+//        if(a==10 && b==20){
+//            System.out.println("Hello World!");
+//        }
+
+
+        //OR
+//        if(a==10 || b==20){
+//            System.out.println("Hello World!");
+//        }
+
+        //NOT
+        if(a!=30){
+           System.out.println("Hello World!");
+       }
+
+
+    }
+}
