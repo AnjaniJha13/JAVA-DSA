38d2eb5f521ae5dc6a49053daa3a1290a1b4d0e5
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-01-31 11:31:01 +0530

    Create Primitives.java

---
+++ Java/Primitives.java
@@ -0,0 +1,20 @@
+public class Primitives{
+    public static void main(String[] args) {
+   /*
+        int rollno = 26;
+        char letter = 'r';
+        float marks = 98.23f;
+        double largeDecimalNumbers = 42525562.546;
+        long largeInteger = 1313445467675664L;
+        boolean check = false;
+        */
+    }
+
+}
+/*
+ Primitive data-types: The input which cannot be breakable further in different data type
+"Anjani" -> 'A'+'n'+'j'+'a'+'n'+'i' - string,char
+string ->  always starts with ""
+26 -> not possible
+
+ */
