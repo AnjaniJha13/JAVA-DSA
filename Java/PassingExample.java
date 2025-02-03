881a1a27554d958b2509f319140b3390917410a2
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-02-03 19:35:48 +0530

    Create PassingExample.java

---
+++ Java/PassingExample.java
@@ -0,0 +1,13 @@
+package com.Anjani;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String name = "Akshay Kumar";
+        greet(name);
+    }
+
+    static void greet(String name) {
+        System.out.println(name);
+    }
+
+}
