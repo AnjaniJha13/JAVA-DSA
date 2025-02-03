80f9d536305de6add0f8632bf4b1f813af7623e2
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-02-03 19:34:01 +0530

    Create Greeting.java

---
+++ Java/Greeting.java
@@ -0,0 +1,11 @@
+package com.Anjani;
+
+public class Greeting {
+    public static void main(StringExample[] args) {
+        greeting();
+    }
+
+    static void greeting() {
+        System.out.println("Hello World!!!");
+    }
+}
