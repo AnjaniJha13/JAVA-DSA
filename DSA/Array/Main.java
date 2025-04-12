d2ad0f0c975fb5695cf6afc7dea05f0083624bcf
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-04-12 21:47:05 +0530

    Create Main.java

---
+++ DSA/Array/Main.java
@@ -0,0 +1,14 @@
+package anjani.com;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] ros;
+        ros = new int [5];
+        //System.out.println(ros[1]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+         //null -> Non-primitive
+        }
+    };
