0aba5195df7f838e47da9246a51d91638ad36404
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-04-25 00:00:22 +0530

    Create Output.java

---
+++ DSA/Strings/Output.java
@@ -0,0 +1,19 @@
+package com.Anjani;
+
+import java.util.Arrays;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(56);
+        Integer num = new Integer(56);
+        System.out.println(num.toString());
+        System.out.println(num);
+        System.out.println("Kunal");
+       // System.out.println(new int[]{2,3,4,5}); // [I@7b23ec81]
+
+      //   System.out.println(Arrays.toString(new int[]{2,3,4,5}));
+
+     //   String name = null;
+     //   System.out.println(name);
+    }
+}
