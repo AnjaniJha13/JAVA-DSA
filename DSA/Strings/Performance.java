2bf8d03e515946b756a09d34ec9354c2037659db
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-04-25 00:00:46 +0530

    Create Performance.java

---
+++ DSA/Strings/Performance.java
@@ -0,0 +1,13 @@
+package com.Anjani;
+
+public class Performance {
+    public static void main(String[] args) {
+        String series = "";
+        for ( int i = 0; i<26; i++) { //0 to 25
+            char ch = (char)('a' + i);
+            System.out.println(i); //(i + "" + ch)
+          //  series = series + ch;
+          //  System.out.println(series);
+        }
+    }
+}
