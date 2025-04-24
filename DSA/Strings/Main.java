356e2364dfa64acb6cf5a7181b5e0dda51c4fef8
Anjani Jha <157398509+AnjaniJha13@users.noreply.github.com>
2025-04-24 23:58:43 +0530

    Create Main.java

---
+++ DSA/Strings/Main.java
@@ -0,0 +1,15 @@
+package com.Anjani;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+     //   int a = 10;
+        String name = "Anjani Jha";
+    //    System.out.println(name);
+
+        String a = "Kunal";
+        System.out.println(a);
+        a = "Kushwaha";
+        System.out.println(a);
+        }
+    }
